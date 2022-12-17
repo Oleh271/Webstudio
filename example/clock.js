5a52b66e7c409d00ca2d2904ad9e247aea5ff13c
Oleh271 <112770065+Oleh271@users.noreply.github.com>
2022-12-17 15:52:30 +0200

    totalHours

--- example/clock.js
+++ example/clock.js
@@ -1,6 +1,6 @@
-let totalMinutes = 450;
+let totalMinutes = 0;
 let hours = Number.parseInt(totalMinutes / 60);
-// hours.padStart(2, "0");
+
 let minutes = totalMinutes - hours * 60;
 
-let totalHours = `${hours.padStart(2, "0")}:${minutes}`;
+let totalHours = `${String(hours).padStart(2, '0')}:${String(minutes).padEnd(2, '0')}`;
