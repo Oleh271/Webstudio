404877b5801b7acb44f858a9a265e742710703ee
Oleh271 <112770065+Oleh271@users.noreply.github.com>
2023-03-17 13:54:27 +0200

    lol

--- example/clock.js
+++ example/clock.js
@@ -4,3 +4,20 @@ let hours = Number.parseInt(totalMinutes / 60);
 let minutes = totalMinutes - hours * 60;
 
 let totalHours = `${String(hours).padStart(2, '0')}:${String(minutes).padEnd(2, '0')}`;
+
+function areArray(arrFirst, arrSecond) {
+    if (arrFirst.length !== arrSecond.length) {
+        return false;
+    }
+    for (i = 0; i <= arrFirst.length; i++) {
+        if (arrFirst[i] !== arrSecond[i]) {
+            return false;
+        }
+        return true;
+    }
+}
+
+let innerText = document.getElementById('form_title');
+console.log(innerText.innerHTML);
+let newText = prompt("Уведіть новий текст.");
+innerText.innerHTML = newText;
--- example/headman.js
+++ example/headman.js
@@ -1,33 +1,57 @@
-const words = ['computer', 'javascript', 'notebook', 'coffee', 'tesla', 'work', 'tea', 'money'];
-const word = words[Math.floor(Math.random() * words.length)];
-
-console.log(word);
-let answerArray = [];
-for (let i = 0; i < word.length; i++) {
-  answerArray[i] = ' _ ';
+function pickWord() {
+  const words = ['computer', 'javascript', 'notebook', 'coffee', 'tesla', 'work', 'tea', 'money'];
+  return words[Math.floor(Math.random() * words.length)];
 }
+function setupAnswerArray(word) {
+  const answerA = [];
+  for (let i = 0; i < word.length; i++) {
+    answerA[i] = ' _ ';
+  }
+  return answerA;
+}
+function showPlayerProgress(answerArray) {
+  alert(answerArray.join(' '));
+}
+function getGuess() {
+  numberRemaining--;
+  return prompt(
+    `Вгадайте букву, чи нажміть Скасувати для вихода із ігри. У вас ${numberRemaining} спроб, щоб вгадати!`
+  );
+}
+function updateGameState(guess, word, answerArray) {
+ let newCorrect = 0;
+  for (let j = 0; j < word.length; j++) {
+    if (word[j] === guess.toLowerCase() && answerArray[j] === ' _ ') {
+      answerArray[j] = guess.toLowerCase();
+      newCorrect++;
+    }
+  }
+  return newCorrect;
+}
+function showAnswerAndCongratulatePlayer(answerArray) {
+  if (numberRemaining > 0) {
+    alert(answerArray.join(' '));
+    alert('Супер! Було загадано слово ' + word);
+  } else {
+    alert('Нажаль ви програли!');
+  }
+}
+
+const word = pickWord();
+console.log(word);
+const answerArray = setupAnswerArray(word);
 let remainingLetters = word.length;
 let numberRemaining = 11;
 while (remainingLetters > 0 && numberRemaining > 0) {
-  alert(answerArray.join(' '));
-  let guess = prompt('Вгадайте букву, чи нажміть Скасувати для вихода із ігри. У вас 10 спроб, щоб вгадати!');
-  numberRemaining--;
+  showPlayerProgress(answerArray);
+  const guess = getGuess();
   if (guess === null) {
     break;
   } else if (guess.length !== 1) {
     alert('Уведіть будь ласка одну букву');
   } else {
-    for (let j = 0; j < word.length; j++) {
-      if (word[j] === guess.toLowerCase() && answerArray[j] === ' _ ') {
-        answerArray[j] = guess.toLowerCase();
-        remainingLetters--;
-      }
-    }
+    let correctGuesses = updateGameState(guess, word, answerArray);
+    remainingLetters -= correctGuesses;
   }
 }
-if (numberRemaining > 0) {
-  alert(answerArray.join(' '));
-  alert('Супер! Було загадано слово ' + word);
-} else {
-  alert('Нажаль ви програли!');
-}
+showAnswerAndCongratulatePlayer(answerArray);
